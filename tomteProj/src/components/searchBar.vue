2b4644977da8ba789545cf71f444cd20ed007c5d
Artemii Kessler <art.kessler2014@yandex.ru>
2020-04-11 14:58:37 +0300

    initial-vue-setup

---
+++ tomteProj/src/components/searchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <!-- <v-input
+      v-model="searchVal"
+    ></v-input> -->
+    <v-text-field
+      v-model="searchVal"
+    ></v-text-field>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'SearchBar',
+
+    data: () => ({
+      searchVal: "aa"
+    }),
+  }
+</script>
---
+++ tomteProj/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <SearchBar class="search" msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<style scoped>
+  .home {
+    grid-template-areas:    
+      "search search"
+      "filters list";
+  }
+
+  .search {
+    grid-area: "search";
+  }
+
+  .filters {
+    grid-area: "filters";
+  }
+
+  .itemList {
+    grid-area: "list";
+  }
+
+</style>
+
+<script>
+import SearchBar from '../components/searchBar';
+
+export default {
+  name: 'Home',
+  components: {
+    SearchBar
+  }
+}
+</script>
